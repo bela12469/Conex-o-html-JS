94abd920671b13eb251e6e9b84dedf30f380a87a
Isabela Cirino <dbela.cirino@outlook.com>
2022-12-20 18:51:25 -0300

    aprendendo como anexar JS com HTML. Primera aula.

---
+++ script.js
@@ -0,0 +1,19 @@
+const formTitle = document.getElementById('text')
+console.log (formTitle)
+console.log (formTitle.innerText)
+formTitle.innerText = 'Expressar sua Opnião. Envie-nos uma mensagem :)'
+
+
+// selecionar o elemento usando o ID: criando uma variavel 
+const redeSociais = document.getElementById('midia')
+
+// printar o conteúdo do elemento no Console web
+console.log (redeSociais.innerText)
+
+// alterar o conteúdo do elemento: nome da variável + innerText
+redeSociais.innerText = "Redes Socias"
+
+
+const comentTitle = document.getElementById('destaque')
+comentTitle.innerText = "Comentários Destacados"
+
